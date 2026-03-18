c6266a13982cca869738207a960eb324c35fc687
Szabo Jozsef <skystone213@gmail.com>
2026-03-18 16:33:24 +0100

    Táblázat elkezdve

---
+++ src/table.js
@@ -0,0 +1,5 @@
+employees = [
+    { id: 1, name: "Péter", city: "Budapest", salary: 1000 },
+    { id: 2, name: "Katalin", city: "Budapest", salary: 1500 },
+    { id: 3, name: "József", city: "Szeged", salary: 2000 },
+]
